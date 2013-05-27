0282526fa04129beba18c8f2e59a39be9d4cb96d
Alexandros Bantis <ambantis@gmail.com>
2013-05-26 17:03:26 -0700

    exercise 1.35

--- sicp-01.03.scm
+++ sicp-01.03.scm
@@ -441,3 +441,6 @@
 ;; Exercise 1.35 Show that the golden ratio Φ is a fixed-point of the
 ;; transformation x ==> 1 + 1/x, and use this fact to compute Φ by means
 ;; of the fixed-point procedure.
+
+(define (golden-ratio x)
+  (fixed-point (lambda (y) (+ 1 (/ 1 y))) 1.0))
