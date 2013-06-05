359b0f17c09689be20c83e0468ef9187965feae6
Alexandros Bantis <ambantis@gmail.com>
2013-06-05 00:48:06 -0700

    exercise 1.41

--- sicp-01.03.scm
+++ sicp-01.03.scm
@@ -829,3 +829,16 @@
 
 ;; (newtons-method (cubic -3 -144 432) 1) -> 3.0
 
+;; Exercise 1.41 Define a procedure *double* that takes a procedure of
+;; one argument and returns a procedure that applies the original procedure
+;; twice. For example, if *inc* is a procedure that adds 1 to its argument,
+;; then (double inc) should be a procedure that adds 2. What value is
+;; returned by
+;;
+;;         (((double (double double)) inc) 5)
+;;
+
+(define (double f)
+  (lambda (x) (f (f x))))
+
+;; (((double (double double)) inc) 5) -> 21
