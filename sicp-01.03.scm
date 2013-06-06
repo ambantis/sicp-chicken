ddb26adbefb1a76ce0d908ae14f6bc9cbd642964
Alexandros Bantis <ambantis@gmail.com>
2013-06-05 20:12:28 -0700

    exercise 1.42

--- sicp-01.03.scm
+++ sicp-01.03.scm
@@ -842,3 +842,13 @@
   (lambda (x) (f (f x))))
 
 ;; (((double (double double)) inc) 5) -> 21
+
+;; Exercise 1.42 Let *f* and *g* be two one argument functions. The
+;; composition f after g is defined to be the function x -> f(g(x)).
+;; Define a procedure *compose* that implements composition. For
+;; example, if *inc* is a procedure that adds 1 to its arguments,
+;;
+;;     ((compose square inc) 6) -> 49
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
