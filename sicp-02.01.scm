b9a9e5783ab10c7a64f010df916cfdb1728f156a
Alexandros Bantis <ambantis@gmail.com>
2014-01-18 23:58:53 -0800

    exercise 2.2

--- sicp-02.01.scm
+++ sicp-02.01.scm
@@ -72,3 +72,60 @@
           (else 1)))
   (let ((g (gcd n d)))
     (cons (* sign (abs (/ n g))) (abs (/ d g)))))
+
+
+;; 2.1.2 Abstraction Barriers
+;; --------------------------
+;;
+;; Generally speaking, we should identify the common set of data set operations that can be applied
+;; to all instances of a data set type and use only those operations. One question would be when to
+;; implement a reduction algorithm, in the constructor or when an operation is called on the number.
+;; One advantage of having an abstract interface is that this choice can be deferred.
+
+;; Exercise 2.2. Consider the problem of representing line segments in a plane. Each segment is
+;; represented as a pair of points: a starting point and an ending point. Define a constructor
+;; `make-segment` and selectors `start-segment` and `end-segment` that define the representation of
+;; segments in terms of points. Furthermore, a point can be represented as a pair of numbers: the
+;; x-coordinate and the y-coordinate. Accordingly, specify a constructor `make-point` and selectors
+;; `x-point` and `y-point` that define this representation. Finally, using your selectors and
+;; constructors, define a procedure `midpoint-segment` that takes a line segment as argument and
+;; returns its midpoint (the point whose coordinates are the average of the coordinates of the
+;; endpoints. To try your procedures, you'll need a way to print points:
+
+(define (print-point p)
+  (printf "(~S,~S)~N"
+          (x-point p)
+          (y-point p)))
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point p)
+  (car p))
+
+(define (y-point p)
+  (cdr p))
+
+(define (make-segment p1 p2)
+  (cons p1 p2))
+
+(define (start-segment s)
+  (car s))
+
+(define (end-segment s)
+  (cdr s))
+
+(define (avg a b)
+  (/ (+ a b) 2))
+
+(define (midpoint-segment s)
+  (let* ((p1 (start-segment s))
+         (p2 (end-segment s))
+         (x1 (x-point p1))
+         (x2 (x-point p2))
+         (y1 (y-point p1))
+         (y2 (y-point p2)))
+    (make-point (avg x1 x2) (avg y1 y2))))
+
+
+
